e6073765ea4886ab3e9644476b412232fc1116b8
Nikhil Karthikeyan <nkarthi@uschill-lab1.cis.udel.edu>
2025-01-23 00:28:23 -0500

    Changes 1. Changed docstring format to sphinx native rst format in module scripts 2. Modified source file addition in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import schiller_lab_tools
+# sys.path.insert(0, os.path.abspath('../../schiller_lab_tools/input_output'))
+# sys.path.insert(0, os.path.abspath('../../schiller_lab_tools/microstructure'))
+# sys.path.insert(0, os.path.abspath('../../schiller_lab_tools/particle'))
 sys.path.insert(0, os.path.abspath('../../schiller_lab_tools'))
 
 # Configuration file for the Sphinx documentation builder.
@@ -14,17 +17,12 @@ sys.path.insert(0, os.path.abspath('../../schiller_lab_tools'))
 project = 'schiller_lab_tools'
 copyright = '2025, Nikhil Karthikeyan'
 author = 'Nikhil Karthikeyan'
-release = schiller_lab_tools.__version__
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.githubpages',
-    'numpydoc',
-]
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 master_doc = 'index'
