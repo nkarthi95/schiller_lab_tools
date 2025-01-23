c5a30680953964b71ddddffe0e98f08c66529bed
Nikhil Karthikeyan <nkarthi@uschill-lab1.cis.udel.edu>
2025-01-23 13:06:16 -0500

    added a testing notebook

---
+++ tests/context.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# import schiller_lab_tools
+
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+import schiller_lab_tools
